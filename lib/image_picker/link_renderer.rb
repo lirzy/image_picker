1638d414751705426a34666217503cab433f302c
Liron <liron@Lirons-MacBook.home>
2009-10-08 12:40:42 +0200

    Paginating and fixing first appearance of image picker

--- lib/image_picker/link_renderer.rb
+++ lib/image_picker/link_renderer.rb
@@ -2,7 +2,7 @@ if defined?(WillPaginate)
   module ImagePicker
     class LinkRenderer < WillPaginate::LinkRenderer
       def page_link(page, text, attributes = {})
-        @template.link_to_function text, "ImagePicker.open_picker({field:$('target_field').value, page:'#{page}', terms:$('terms').value});"
+        @template.link_to_function text, "ImagePicker.open_picker({field:$('target_field').value, page:'#{page}', url:$('image_picker_url').value});"
       end
     end
   end
